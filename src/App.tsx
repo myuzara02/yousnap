3f300a09a3ba358d235b982c0d0b18e072bd2742
Miftah Yuzar Azwardhi <myuzara12@gmail.com>
2023-12-06 11:56:59 +0700

    add leftsidebar

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,14 @@ const App = () => {
                 {/* Private Route */}
                 <Route element={<RootLayout />}>
                     <Route index element={<Home />} />
+                    <Route path='/explore' element={<Explore />} />
+                    <Route path='/saved' element={<Saved />} />
+                    <Route path='/all-users' element={<AllUsers />} />
+                    <Route path='/create-post' element={<CreatePost />} />
+                    <Route path='/update-post/:id' element={<EditPost />} />
+                    <Route path='/post/:id' element={<PostDeatails />} />
+                    <Route path='/profile/:id/*' element={<Profile />} />
+                    <Route path='/update-profile/:id' element={<UpdateProfile />} />
                 </Route>
             </Routes>
 
