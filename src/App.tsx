71dddefcde030353f03d807f3c290e29d70cd834
Miftah Yuzar Azwardhi <myuzara12@gmail.com>
2023-12-06 12:10:03 +0700

    fix home section

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import SigninForm from './_auth/forms/SigninForm'
 import SignupForm from './_auth/forms/SignupForm'
-import { Home } from './_root/pages'
+import { AllUsers, CreatePost, EditPost, Explore, Home, Profile, Saved, UpdateProfile, PostDetails } from './_root/pages'
 import AuthLayout from './_auth/AuthLayout'
 import RootLayout from './_root/RootLayout'
 import { Toaster } from "@/components/ui/toaster"
@@ -30,7 +30,7 @@ const App = () => {
                     <Route path='/all-users' element={<AllUsers />} />
                     <Route path='/create-post' element={<CreatePost />} />
                     <Route path='/update-post/:id' element={<EditPost />} />
-                    <Route path='/post/:id' element={<PostDeatails />} />
+                    <Route path='/post/:id' element={<PostDetails />} />
                     <Route path='/profile/:id/*' element={<Profile />} />
                     <Route path='/update-profile/:id' element={<UpdateProfile />} />
                 </Route>
